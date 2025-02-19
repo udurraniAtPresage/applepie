97e52089e2479a450a95d99dc6e9a87a31ed533f
udurrani <udurrani@presagegroup.com>
2025-02-19 14:58:26 -0500

    vcr

--- tests/testthat/test-packages.R
+++ tests/testthat/test-packages.R
@@ -1,4 +1,6 @@
-test_that("get_packages() works", {
-  pkgs <- get_packages("maelle")
+test_that("get_packages() works in vcr", {
+  vcr::use_cassette("packages", {
+    pkgs <- get_packages("ggseg")
+  })
   expect_vector(pkgs, character())
 })
