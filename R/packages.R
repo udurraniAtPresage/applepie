cbb6a3786dd728ebf09649064ed6a3af843b961f
udurraniAtPresage <142413619+udurraniAtPresage@users.noreply.github.com>
2025-02-19 13:17:17 -0500

    first commit from umair

--- R/packages.R
+++ R/packages.R
@@ -8,9 +8,14 @@
 #' @examples
 #' get_packages("ropensci")
 get_packages <- function(universe) {
-  url <- sprintf("https://%s.r-universe.dev/packages", universe)
-  resp <- httr::RETRY("GET", url)
-  httr::stop_for_status(resp, "take off")
-  content <- httr::content(resp)
-  unlist(content)
+  # url <- sprintf("https://%s.r-universe.dev/packages", universe)
+  # resp <- httr::RETRY("GET", url)
+  # httr::stop_for_status(resp, "take off")
+  # content <- httr::content(resp)
+  # unlist(content)
+  url <- sprintf("https://%s.r-universe.dev", universe)
+  httr2::request(paste0(url, "/api/ls")) |>
+    httr2::req_user_agent("R-universe docs") |>
+    httr2::req_perform() |>
+    httr2::resp_body_json() |> unlist()
 }
